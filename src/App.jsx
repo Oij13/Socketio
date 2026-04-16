a7273bbb6a20d4f7f934857f5b77afeea221f5b9
Owen Johnson <oij13@icloud.com>
2026-04-16 16:07:30 -0400

    feat: initial socket.io chat implementation

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Blog } from './pages/Blog.jsx'
+import { Signup } from './pages/Signup.jsx'
+import { Login } from './pages/Login.jsx'
+import { AuthContextProvider } from './contexts/AuthContext.jsx'
+import { io } from 'socket.io-client'
+
+const socket = io(import.meta.env.VITE_SOCKET_HOST, {
+  query: window.location.search.substring(1),
+  auth: {
+    token: window.localStorage.getItem('token'),
+  },
+})
+
+socket.on('connect', async () => {
+  console.log('connected to socket.io as', socket.id)
+  socket.emit('chat.message', 'hello from client')
+  const userInfo = await socket.emitWithAck('user.info', socket.id)
+  console.log('user info', userInfo)
+})
+
+socket.on('connect_error', (err) => {
+  console.error('socket.io connect error:', err)
+})
+
+socket.on('chat.message', (msg) => {
+  console.log(`${msg.username}: ${msg.message}`)
+})
+
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Blog />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+])
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </QueryClientProvider>
+  )
+}
